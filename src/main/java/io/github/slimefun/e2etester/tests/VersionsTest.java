dae07467331eb52aff595e498fc35c99bd5e3a57
Daniel Walsh <walshydev@gmail.com>
2023-07-05 00:09:36 +0100

    Update tests for matrix

--- src/main/java/io/github/slimefun/e2etester/tests/VersionsTest.java
+++ src/main/java/io/github/slimefun/e2etester/tests/VersionsTest.java
@@ -1,5 +1,7 @@
 package io.github.slimefun.e2etester.tests;
 
+import org.bukkit.Bukkit;
+
 import io.github.slimefun.e2etester.framework.Assert;
 import io.github.slimefun.e2etester.framework.annotations.E2ETest;
 
@@ -7,7 +9,7 @@ public class VersionsTest {
 
 	@E2ETest(description = "Test that `/sf versions` returns the right MC version")
 	public void testMcVersion() {
-		Assert.runConsoleCommand("sf versions", (output) -> output.contains("MC: 1.20.1"));
+		Assert.runConsoleCommand("sf versions", (output) -> output.contains("Paper " + Bukkit.getVersion()));
 	}
 
 	@E2ETest(description = "Test that `/sf versions` returns the right SF version")
@@ -17,6 +19,17 @@ public class VersionsTest {
 
 	@E2ETest(description = "Test that `/sf versions` returns the right Java version")
 	public void testJavaVersion() {
-		Assert.runConsoleCommand("sf versions", (output) -> output.contains("Java 20"));
+		// Grab the Java version, if it's still a 1.x strip that and then remove minor so we go from 1.8.1 -> 8
+		String expectedVersion = System.getProperty("java.version");
+		if (expectedVersion.startsWith("1.")) {
+			expectedVersion = expectedVersion.substring(2);
+		}
+		int minorVersionIdx = expectedVersion.indexOf('.');
+		if (minorVersionIdx != -1) {
+			expectedVersion = expectedVersion.substring(0, minorVersionIdx);
+		}
+
+		final String finalExpectedVersion = expectedVersion;
+		Assert.runConsoleCommand("sf versions", (output) -> output.contains("Java " + finalExpectedVersion));
 	}
 }
